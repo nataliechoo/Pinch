e86198dbe177ec7847ef29845109d8550bbf4ae7
Natalie <njchoo0201@gmail.com>
2022-07-22 18:52:44 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Natalie Choo on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
